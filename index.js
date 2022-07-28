975c2de5dc10cb7e3567a1ab9a1a8b057a177b43
Julian Esteban Rios Holguin <jerios84@misena.edu.co>
2022-07-27 21:16:16 -0500

    Docker compose

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const path = require('path');
-const port = process.env.PORT || 5000 ;
+const port = 5000 ;
 
 const app = express();
 
